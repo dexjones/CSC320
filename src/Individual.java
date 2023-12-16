4a8fe1508b0bc214178a2ce4abe9ac5f580ecefa
Dexter <dexter.t.jones@outlook.com>
2023-12-16 15:54:35 -0700

    Structured correctly. Prints predefined variables

--- src/Individual.java
+++ src/Individual.java
@@ -1,9 +1,40 @@
+/*
+Written by Dexter Jones
+Date: 12/16/2023
+
+Pseudocode:
+An application that will print the following information for a fictional character:
+
+User input:
+First Name (string)
+Last Name (string)
+Street Address (string)
+City (string)
+Zip code (int)
+
+Output:
+Result (combination of the above information)
+ */
+
+import java.util.Scanner;
+
 public class Individual {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        String fName, lName, address, city;
+        int zip;
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        System.out.println("Fictional Character Information");
+
+        fName = "Dexter";
+        lName = "Jones";
+        address = "123 Sunshine Road";
+        city = "Laramie";
+        zip = 82072;
+
+        System.out.println(fName);
+        System.out.println(lName);
+        System.out.println(address);
+        System.out.println(city);
+        System.out.println(zip);
     }
 }
