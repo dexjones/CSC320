f183752d3dc9a20b77dfb4a6de7bb521841fbbe5
Dexter <dexter.t.jones@outlook.com>
2023-12-16 16:43:42 -0700

    Structured correctly. Asks for user input

--- src/Individual.java
+++ src/Individual.java
@@ -10,7 +10,7 @@ First Name (string)
 Last Name (string)
 Street Address (string)
 City (string)
-Zip code (int)
+Zip code (string)
 
 Output:
 Result (combination of the above information)
@@ -20,21 +20,41 @@ import java.util.Scanner;
 
 public class Individual {
     public static void main(String[] args) {
-        String fName, lName, address, city;
-        int zip;
+        Scanner scnr = new Scanner(System.in);
+        String fName, lName, address, city, zip;
+        String result[] = new String[5];
 
-        System.out.println("Fictional Character Information");
+        System.out.println("Fictional Character Information\n");
 
-        fName = "Dexter";
-        lName = "Jones";
-        address = "123 Sunshine Road";
-        city = "Laramie";
-        zip = 82072;
+        System.out.println("First name: ");
+        fName = scnr.nextLine();
+        result[0] = fName;
+        System.out.println("Last name: ");
+        lName = scnr.nextLine();
+        result[1] = lName;
+        System.out.println("Street address: ");
+        address = scnr.nextLine();
+        result[2] = address;
+        System.out.println("City: ");
+        city = scnr.nextLine();
+        result[3] = city;
+        System.out.println("Zip code: ");
+        zip = scnr.nextLine();
+        result[4] = zip;
 
-        System.out.println(fName);
-        System.out.println(lName);
-        System.out.println(address);
-        System.out.println(city);
-        System.out.println(zip);
+        System.out.println("The fictional character information is: ");
+        for (int i = 0; i < result.length; i++) {
+            if (i == 0)
+                System.out.print("First Name: ");
+            else if (i == 1)
+                System.out.print("Last Name: ");
+            else if (i == 2)
+                System.out.print("Street Address: ");
+            else if (i == 3)
+                System.out.print("City: ");
+            else
+                System.out.print("Zip Code: ");
+            System.out.println(result[i]);
+        }
     }
 }
